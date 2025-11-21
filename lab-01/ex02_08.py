dd4db2cd27114d89974b5a8aef46947d6e82a7b1
lamhung <lhung89030@gmail.com>
2025-11-21 10:29:46 +0700

    [add] ex03

--- lab-01/ex02_08.py
+++ lab-01/ex02_08.py
@@ -2,7 +2,7 @@ def chia_het_cho_5(so_nhi_phan):
     so_thap_phan = int(so_nhi_phan, 2)
     if so_thap_phan % 5 ==0:
         return True
-    else
+    else:
         return False
 chuoi_so_nhi_phan = input("nhap chuoi so nhi phan: ")    
 so_nhi_phan_list = chuoi_so_nhi_phan.split(',')
---
+++ lab-01/ex03/ex03_01.py
@@ -0,0 +1,12 @@
+def tinh_tong_so_chan(lst):
+    tong = 0
+    for num in lst:
+        if num % 2 ==0:
+            tong +=num
+        return tong
+    
+input_list = input("Nhap so,cach nhau dau phay: ")
+numbers =list(map(int,input_list.split(",")))
+
+tong_chan = tinh_tong_so_chan(numbers)
+print("tong so trong lst: ", tong_chan)
