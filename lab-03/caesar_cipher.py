42471af1923a9c16ea1481ca9f8c8d6aec72221c
lamhung <lhung89030@gmail.com>
2025-12-12 08:27:29 +0700

    Add folder

--- lab-03/caesar_cipher.py
+++ lab-03/caesar_cipher.py
@@ -9,11 +9,9 @@ class MyApp(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-       
         self.ui.pushButton.clicked.connect(self.call_api_encrypt)
         self.ui.pushButton_2.clicked.connect(self.call_api_decrypt)
 
-   
     def call_api_encrypt(self):
         url = "http://127.0.0.1:5000/api/caesar/encrypt"
 
@@ -26,39 +24,31 @@ class MyApp(QMainWindow):
             response = requests.post(url, json=payload)
             if response.status_code == 200:
                 data = response.json()
-
-               
                 self.ui.plainTextEdit_2.setPlainText(data["encrypted_message"])
-
                 QMessageBox.information(self, "Success", "Encrypted Success")
             else:
                 QMessageBox.warning(self, "Error", "Error calling API")
         except Exception as e:
             QMessageBox.warning(self, "Error", str(e))
 
-  
     def call_api_decrypt(self):
         url = "http://127.0.0.1:5000/api/caesar/decrypt"
 
         payload = {
-            "plain_text": self.ui.plainTextEdit_2.toPlainText(),
-            "key": self.ui.lineEdit.text()
+            "cipher_text": self.ui.plainTextEdit_2.toPlainText(),
+            "key": int(self.ui.lineEdit.text())
         }
 
         try:
             response = requests.post(url, json=payload)
             if response.status_code == 200:
                 data = response.json()
-
-                
                 self.ui.plainTextEdit.setPlainText(data["decrypted_message"])
-
-                QMessageBox.information(self, "Success", "Decrypted Success")
+                QMessageBox.information(self, "Success", "Decrypted Success")  # Fixed
             else:
                 QMessageBox.warning(self, "Error", "Error calling API")
         except Exception as e:
-            QMessageBox.warning(self, "Error", str(e))
-
+            QMessageBox.warning(self, "Error", str(e))  # Fixed indent
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
